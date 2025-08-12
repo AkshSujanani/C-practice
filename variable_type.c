dcc6de3cfab41f4910d36df6299bc384804852cf
AkshSujanani <akshsujanani1@gmail.com>
2025-08-12 21:02:32 +0530

    file updated variable

--- variable_type.c
+++ variable_type.c
@@ -9,7 +9,7 @@ int main(){
         printf("%c is a Capital letter" , x);
     else if(x >= 97 && x<= 122)
         printf("%c is a small alphabet" , x);
-    else if(x >=0 && x<=9)
+    else if(x >='0' && x<='9')
         printf("%c is a number" , x);
     else if((x >= 0 && x <= 47) || (x >= 58 && x<= 64) || (x >= 91 && x <= 96) || (x >= 123 && x <= 127))
         printf("%c is a special symbol" , x);
