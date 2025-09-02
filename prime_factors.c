d191528de27938d28d8360052a8362e99707bd03
AkshSujanani <akshsujanani1@gmail.com>
2025-09-02 16:49:38 +0000

    Good Question learned a lot things in it(added in comments)

---
+++ prime_factors.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int prime(int i){
+    int j,pr=1;
+    if(i == 2)
+        pr = 1;
+    else{
+        for(j=2 ; j<i ; j++)
+            if(i%j == 0)
+                pr = 0;
+    }
+
+    if(pr == 1)
+        return 1;
+    else
+        return 0;
+}
+
+int prime_factor(int num){
+    int i;
+    i=2;
+
+    while(num > 1){
+        if(prime(i))
+            while((num%i) == 0){
+                num /= i;
+                printf("%d\t" , i);
+            }
+        i++;
+    }
+    return 0;
+}
+
+int main(){
+    int num;
+    printf("Enter the number whose prime factors you want to find: ");
+    scanf("%d" , &num);
+    printf("\nPrime factors of %d:\n" , num);
+    prime_factor(num);
+    return 0;
+}
